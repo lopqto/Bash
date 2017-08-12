2e4cecb75b71f2d66bca923f41f1eee722e843f9
Hamidreza Babaee <morpix@protonmail.com>
2017-08-12 19:00:35 +0430

    add script - dnsmasq-server

---
+++ dnsmasq-server/script.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -e
+
+
+# Must be run as root
+if [[ `whoami` != "root" ]]
+then
+  echo "This install must be run as root or with sudo."
+  exit
+fi
+
+apt install -y dnsmasq
+cat - > /etc/dnsmasq.conf <<DNSMASQCONF
+# dnsmasq.conf for raspberry pi    
+# Details found here:
+# http://lopqto.net/2017/08/12/install-dnsmasq-on-raspberry-pi/  
+
+domain-needed
+no-resolv
+server=8.8.8.8
+server=8.8.4.4
+
+cache-size=4096
+domain=raspberry.local
+
+DNSMASQCONF
+
+service dnsmasq restart
+
+echo 'Installation complete. Enjoy!'
